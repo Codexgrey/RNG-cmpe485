a81c6192f8a82e9c8c3fb2cd08ef41d150f943ec
Collins Ovuakporaye <thecodexgrey@gmail.com>
2025-03-03 23:48:56 +0200

    generating 100 integers, converting them to random numbers, running simulation

--- sim.c
+++ sim.c
@@ -1,18 +1,70 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
+
 
 // function prototypes
-int rand(void);
+double randNum(void);
 void srand(unsigned int seed);
 
 
 // main function
 int main(void){
-    printf("random num is %d", rand());
+    int n, n1 = 0, n2 = 0, n3 = 0, ntotal;
+    double r;
+    /*
+        service time : probability : cumulative probability
+        Ts-1 => 4 : 0.35 : 0.35
+        Ts-2 => 8 : 0.50 : 0.85
+        Ts-3 => 12 : 0.15 : 1.00
+    */
+
+    // seed random number generator with current time
+    srand(time(0)); 
+
+    // loop to generate random numbers
+    for (n=1; n<=100; n++){
+        r = randNum();
+
+        if (r <= 0.35) {
+            n1 += 1;
+            printf("Random number %d -> assigned Ts (Service time) -> Ts-1 ( 4 mins.) \n", n);
+        }
+        else if (r > 0.35 && r <= 0.85) {
+            n2 += 1;
+            printf("Random number %d -> assigned Ts (Service time) -> Ts-2 ( 8 mins.) \n", n);
+        }
+        else {
+            n3 += 1;
+            printf("Random number %d -> assigned Ts (Service time) -> Ts-3 (12 mins.) \n", n);
+        }
+    }
+
+
+    // summary of results
+    printf("\n Number of each Ts Type: \n"
+        "Ts-1: %d \n"
+        "Ts-2: %d \n"
+        "Ts-3: %d \n", n1, n2, n3);
+
+    // calculate and print proportions of each Ts Type
+    ntotal = n1 + n2 + n3;
+    double p1, p2, p3;
+
+    p1 = ((double) n1 / ntotal) * 100;
+    p2 = ((double) n2 / ntotal) * 100;
+    p3 = ((double) n3 / ntotal) * 100;
+
+    printf("\n Proportion of each Ts Type: \n"
+        "Ts-1: %f \n"
+        "Ts-2: %f \n"
+        "Ts-3: %f \n", p1, p2, p3);
+
 
     return 0;
 }
 
-int rand(void){
-    return RAND_MAX;
+// generate random number
+double randNum(void){
+  return  ((double) rand()/RAND_MAX);
 }
